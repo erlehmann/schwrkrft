de633452202053ffec5eb8db35dac326357381e5
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-11-29 23:52:33 +0100

    + AGPL license

--- js/n-body.js
+++ js/n-body.js
@@ -1,3 +1,21 @@
+/*
+    n-body.js - javascript n body simulation
+    Copyright (C) 2010 erlehmann
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var Universe = function(canvas, milliseconds, offset) {
     this.addPointMass = function(position, velocity, mass) {
         this.particles.push(new PointMass(this, position, velocity, mass));
