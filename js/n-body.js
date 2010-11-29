e3a535f52bae72f6e0704a86042dc79e1726e75c
Nils Dagsson Moskopp <nils@dieweltistgarnichtso.net>
2010-11-30 00:05:56 +0100

    * more AGPL

--- js/n-body.js
+++ js/n-body.js
@@ -1,6 +1,6 @@
 /*
-    n-body.js - javascript n body simulation
-    Copyright (C) 2010 erlehmann
+    n-body.js — javascript n-body simulation
+    Copyright (C) 2010  erlehmann
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
